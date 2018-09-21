eeb0e5c73a9956be2fbca9e364435f7f5e3ecf0b
suguru sugiyama <sugi.mount@gmail.com>
2018-09-21 20:45:12 +0900

    add QuestionCreateHandler(文字列出力するだけ)

--- qicoo_api.go
+++ qicoo_api.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	_ "fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func QuestionCreateHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Gorilla!\n"))
+}
 
 func main() {
-	fmt.Println("vim-go")
+	r := mux.NewRouter()
+
+	// route QuestionCreate
+	r.Path("/v1/{event_id}/questions").
+		Methods("POST").
+		HandlerFunc(QuestionCreateHandler)
+
+	http.ListenAndServe(":8080", r)
 }
