968fedd302b237f81b69126d107c7a18d9061771
suguru sugiyama <sugi.mount@gmail.com>
2018-09-22 02:07:23 +0900

    add MySQL Connection

--- qicoo_api.go
+++ qicoo_api.go
@@ -3,12 +3,16 @@ package main
 import (
 	_ "bytes"
 	"encoding/json"
-	_ "fmt"
+	"fmt"
 	_ "log"
 	"net/http"
+	"os"
+	_ "strconv"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 // Question Questionオブジェクトを扱うためのstruct
@@ -27,18 +31,39 @@ type Question struct {
 // QuestionCreateHandler QuestionオブジェクトをDBとRedisに書き込む
 func QuestionCreateHandler(w http.ResponseWriter, r *http.Request) {
 
-	// DBとRedisに書き込むためのstiruct Object を生成
+	// DBとRedisに書き込むためのstiruct Object を生成。POST REQUEST のBodyから値を取得
 	var question Question
 	decoder := json.NewDecoder(r.Body)
 	decoder.Decode(&question)
 
-	// uuidを新たに生成
+	// POST REQUEST の BODY に含まれていない値の生成
 	newUUID := uuid.New()
 	question.ID = newUUID.String()
-	//fmt.Println(newUUID.String())
 
-	w.Write([]byte("comment:" + question.Comment + " ID:" + question.ID + "\n"))
-	//question := &Question{ID: "testid"}
+	question.Object = "question"
+
+	// TODO: Cookieからsessionidを取得して、Redisに存在する場合は、usernameを取得してquestionオブジェクトに格納する
+
+	// URLに含まれている event_id を取得して、questionオブジェクトに格納
+	vars := mux.Vars(r)
+	eventID := vars["event_id"]
+	question.EventID = eventID
+
+	question.Like = 0
+
+	connection := DbConnect()
+
+	QuestionsEx := []Question{}
+	connection.Find(&QuestionsEx, "event_id=?", "jkd1812")
+	fmt.Println(QuestionsEx)
+
+	// debug
+	//	w.Write([]byte("comment: " + question.Comment + "\n" +
+	//		"ID: " + question.ID + "\n" +
+	//		"Object: " + question.Object + "\n" +
+	//		"eventID: " + question.EventID + "\n" +
+	//		"programID: " + question.ProgramID + "\n" +
+	//		"Like: " + strconv.Itoa(question.Like) + "\n"))
 
 	//var buf bytes.Buffer
 	//enc := json.NewEncoder(&buf)
@@ -49,9 +74,27 @@ func QuestionCreateHandler(w http.ResponseWriter, r *http.Request) {
 	//fmt.Println(buf.String())
 }
 
-// createUUID uuidを生成
-// func createUUID() {
+// DbConnect 環境変数を利用しDBへのConnectionを取得する
+func DbConnect() *gorm.DB {
+	dbms := "mysql"
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	protocol := "tcp(" + os.Getenv("DB_URL") + ")"
+	dbname := "qicoo"
 
+	connect := user + ":" + password + "@" + protocol + "/" + dbname
+	fmt.Println("dbms: " + dbms)
+	fmt.Println("connect: " + connect)
+	db, err := gorm.Open(dbms, connect)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
+//func DbQuestionCreate() {
+//
 //}
 
 func main() {
