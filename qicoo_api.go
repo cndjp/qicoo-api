3cdf634d4122d37661221c4d3f7324ae56f38c18
suguru sugiyama <sugi.mount@gmail.com>
2018-09-21 21:56:22 +0900

    catch post body request

--- qicoo_api.go
+++ qicoo_api.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"bytes"
+	_ "bytes"
 	"encoding/json"
-	"fmt"
-	"log"
+	_ "fmt"
+	_ "log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -12,20 +12,32 @@ import (
 
 // Question Questionオブジェクトを扱うためのstruct
 type Question struct {
-	ID string `json:"id"`
+	ID        string `json:"id"`
+	Object    string `json:"object"`
+	Username  string `json:"username"`
+	EventID   string `json:"event_id"`
+	ProgramID string `json:"program_id"`
+	Comment   string `json:"comment"`
+	CreatedAt string `json:"created_at"`
+	UpdateAt  string `json:"update_at"`
+	Like      int    `json:"like"`
 }
 
 // QuestionCreateHandler QuestionオブジェクトをDBとRedisに書き込む
 func QuestionCreateHandler(w http.ResponseWriter, r *http.Request) {
-	question := &Question{ID: "testid"}
+	decoder := json.NewDecoder(r.Body)
+	var question Question
+	decoder.Decode(&question)
+	w.Write([]byte("comment " + question.Comment))
+	//question := &Question{ID: "testid"}
 
-	var buf bytes.Buffer
-	enc := json.NewEncoder(&buf)
-	if err := enc.Encode(question); err != nil {
-		log.Fatal(err)
-	}
+	//var buf bytes.Buffer
+	//enc := json.NewEncoder(&buf)
+	//if err := enc.Encode(question); err != nil {
+	//	log.Fatal(err)
+	//}
 
-	fmt.Println(buf.String())
+	//fmt.Println(buf.String())
 }
 
 func main() {
