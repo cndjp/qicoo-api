09f37e31ab189d9ff8f00120386a505a6fe9a1a9
nnao45 <n4sekai5y@gmail.com>
2018-10-02 18:17:51 +0900

    sparate symc redis

--- src/handler/list.go
+++ src/handler/list.go
@@ -3,17 +3,13 @@ package handler
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"os"
 	"strconv"
 	"time"
 
-	"github.com/cndjp/qicoo-api/src/sql"
-	"github.com/go-gorp/gorp"
 	"github.com/gomodule/redigo/redis"
 	"github.com/gorilla/mux"
-	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
 
@@ -112,7 +108,7 @@ func (p *RedisPool) GetQuestionList(eventID string, start int, end int, sort str
 	hasCreatedSortedKey := redisHasKey(redisConn, createdSortedKey)
 
 	if !hasQuestionsKey || !hasLikeSortedKey || !hasCreatedSortedKey {
-		p.syncQuestion(eventID)
+		p.SyncQuestion(eventID)
 	}
 
 	/* Redisからデータを取得する */
@@ -179,69 +175,6 @@ func (p *RedisPool) GetQuestionList(eventID string, start int, end int, sort str
 	questionList.Object = "list"
 	questionList.Type = "question"
 	return questionList
-}
-
-func (p *RedisPool) syncQuestion(eventID string) {
-	redisConnection := p.getRedisConnection()
-	defer redisConnection.Close()
-
-	// DBからデータを取得
-	//var m sql.DBMap
-	//dbmap, err := sql.InitMySQLDB()
-	//err := m.InitMySQLDB()
-	var m *gorp.DbMap
-	db, err := sql.InitMySQL()
-	if err != nil {
-		logrus.Error(err)
-		return
-	}
-
-	m = sql.MappingDBandTable(db)
-
-	//dbmap.AddTableWithName(Question{}, "questions")
-	m.AddTableWithName(Question{}, "questions")
-	//defer dbmap.Db.Close()
-	defer m.Db.Close()
-
-	if err != nil {
-		causeErr := errors.Cause(err)
-		fmt.Printf("%+v", causeErr)
-		return
-	}
-
-	var questions []Question
-	//_, err = dbmap.Select(&questions, "SELECT * FROM questions WHERE event_id = '"+eventID+"'")
-	_, err = m.Select(&questions, "SELECT * FROM questions WHERE event_id = '"+eventID+"'")
-
-	if err != nil {
-		causeErr := errors.Cause(err)
-		fmt.Printf("%+v", causeErr)
-		return
-	}
-
-	// DB or Redis から取得したデータのtimezoneをUTCからAsia/Tokyoと指定
-	locationTokyo, err := time.LoadLocation("Asia/Tokyo")
-	for i := range questions {
-		questions[i].CreatedAt = questions[i].CreatedAt.In(locationTokyo)
-		questions[i].UpdatedAt = questions[i].UpdatedAt.In(locationTokyo)
-	}
-
-	//Redisで利用するKeyを取得
-	questionsKey, likeSortedKey, createdSortedKey := getQuestionsKey(eventID)
-
-	//DBのデータをRedisに同期する。
-	for _, question := range questions {
-		//HashMap SerializedされたJSONデータを格納
-		serializedJSON, _ := json.Marshal(question)
-		//fmt.Println(questionsKey, " ", question.ID, " ", string(serializedJSON))
-		redisConnection.Do("HSET", questionsKey, question.ID, serializedJSON)
-
-		//SortedSet(Like)
-		redisConnection.Do("ZADD", likeSortedKey, question.Like, question.ID)
-
-		//SortedSet(CreatedAt)
-		redisConnection.Do("ZADD", createdSortedKey, question.CreatedAt.Unix(), question.ID)
-	}
 }
 
 func getQuestionsKey(eventID string) (questionsKey string, likeSortedKey string, createdSortedKey string) {
---
+++ src/handler/sync.go
@@ -0,0 +1,75 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/cndjp/qicoo-api/src/sql"
+	"github.com/go-gorp/gorp"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+)
+
+func (p *RedisPool) SyncQuestion(eventID string) {
+	redisConnection := p.getRedisConnection()
+	defer redisConnection.Close()
+
+	// DBからデータを取得
+	//var m sql.DBMap
+	//dbmap, err := sql.InitMySQLDB()
+	//err := m.InitMySQLDB()
+	var m *gorp.DbMap
+	db, err := sql.InitMySQL()
+	if err != nil {
+		logrus.Error(err)
+		return
+	}
+
+	m = sql.MappingDBandTable(db)
+
+	//dbmap.AddTableWithName(Question{}, "questions")
+	m.AddTableWithName(Question{}, "questions")
+	//defer dbmap.Db.Close()
+	defer m.Db.Close()
+
+	if err != nil {
+		causeErr := errors.Cause(err)
+		fmt.Printf("%+v", causeErr)
+		return
+	}
+
+	var questions []Question
+	//_, err = dbmap.Select(&questions, "SELECT * FROM questions WHERE event_id = '"+eventID+"'")
+	_, err = m.Select(&questions, "SELECT * FROM questions WHERE event_id = '"+eventID+"'")
+
+	if err != nil {
+		causeErr := errors.Cause(err)
+		fmt.Printf("%+v", causeErr)
+		return
+	}
+
+	// DB or Redis から取得したデータのtimezoneをUTCからAsia/Tokyoと指定
+	locationTokyo, err := time.LoadLocation("Asia/Tokyo")
+	for i := range questions {
+		questions[i].CreatedAt = questions[i].CreatedAt.In(locationTokyo)
+		questions[i].UpdatedAt = questions[i].UpdatedAt.In(locationTokyo)
+	}
+
+	//Redisで利用するKeyを取得
+	questionsKey, likeSortedKey, createdSortedKey := getQuestionsKey(eventID)
+
+	//DBのデータをRedisに同期する。
+	for _, question := range questions {
+		//HashMap SerializedされたJSONデータを格納
+		serializedJSON, _ := json.Marshal(question)
+		//fmt.Println(questionsKey, " ", question.ID, " ", string(serializedJSON))
+		redisConnection.Do("HSET", questionsKey, question.ID, serializedJSON)
+
+		//SortedSet(Like)
+		redisConnection.Do("ZADD", likeSortedKey, question.Like, question.ID)
+
+		//SortedSet(CreatedAt)
+		redisConnection.Do("ZADD", createdSortedKey, question.CreatedAt.Unix(), question.ID)
+	}
+}
