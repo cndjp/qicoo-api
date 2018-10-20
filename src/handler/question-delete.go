0568d1599d00c854b71a7b59486fc11894ef696f
suguru sugiyama <sugi.mount@gmail.com>
2018-10-21 07:42:40 +0900

    CORS対応

--- src/handler/question-delete.go
+++ src/handler/question-delete.go
@@ -21,6 +21,11 @@ type QuestionDeleteResponse struct {
 
 // QuestionDeleteHandler Questionの削除用 関数
 func QuestionDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	// Headerの設定
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
 	// URLに含まれているパラメータを取得
 	vars := mux.Vars(r)
 
